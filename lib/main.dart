38848cc754f580beb8ff2b65bc7b6338b53481ff
abigailsheldon <abigail.l.sheldon@gmail.com>
2025-04-02 17:57:07 -0400

    Adds dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 // Import Flutter packages
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() {
